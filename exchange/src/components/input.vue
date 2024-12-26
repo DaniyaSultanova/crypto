66c397e7204f6303d5d2218d5b0646a66c0a11de
Daniya <d17810049@gmail.com>
2024-12-27 03:35:11 +0500

    crypto

---
+++ exchange/src/components/input.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+    props: {
+        changeAmount: {
+            type: Function,
+            required: true 
+        },
+         convert: {
+            type: Function,
+            required: true 
+        }
+    }
+}
+
+
+</script>
+
+<template>
+  <input type="number" @input="changeAmount($event.target.value)" placeholder="Введите число"><br>
+  <button @click="convert()">Конвертировать</button>
+</template>
+
+<style scoped>
+input {
+    outline: none;
+    width: 500px;
+    position: relative;
+    top: -30px;
+    font-size: 1.2em;
+    border-radius: 3px;
+    border:0;
+    padding:10px 15px;
+}
+
+button {
+    padding: 15px 20px;
+    color: #fff;
+    text-transform: uppercase;
+    cursor: pointer;
+    background: #fbb606;
+    border: 0;
+    border-radius: 3px;
+
+}
+</style>
